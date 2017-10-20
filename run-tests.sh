5def08adf56d3b4ec993c8bd5f69d7ca85596527
Karsten Nikolai Strand <karsten@fusetools.com>
2017-10-20 11:02:37 +0200

    WIP FIXING RUN TESTS SCRIPT

--- run-tests.sh
+++ run-tests.sh
@@ -6,19 +6,30 @@ PREDICATE="cat != TODO"
 CONFIG=Debug
 MSBUILD=msbuild
 NUGET=nuget
+ARG_PREFIX=
+NUNIT_EXTRA_ARGS=
 
 for arg in "$@"; do
     case $arg in
+    --one-worker)
+        NUNIT_EXTRA_ARGS="$NUNIT_EXTRA_ARGS --workers=1"
+        ;;
     --debug)
         CONFIG=Debug
         ;;
     --release)
         CONFIG=Release
         ;;
+    --build)
+        ALWAYS_BUILD=1
+        ;;
     -h|--help)
         echo "Available options:"
-        echo "    --release   Uses release configuration"
-        echo "    --debug     Uses debug configuration (default)"
+        echo "    --release           Uses release configuration"
+        echo "    --debug             Uses debug configuration (default)"
+        echo "    --one-worker        Disable parallel test running"
+        echo "    --build             Build before running tests, even if assemblies already exists"
+        echo "                        If assemblies don't exist they will be built anyway."
         exit 0
         ;;
     *)
@@ -36,6 +47,9 @@ case "$(uname -s)" in
         if [ ! -f $vswhere ]; then
             "$NUGET" install vswhere -ExcludeVersion -Output packages
         fi
+        # Command line flags starting with / have to be prefixed with another / in MSYS
+        # If we don't do that they will be seen as a path and converted to Windows path
+        ARG_PREFIX=/
         MSBUILD="$(cygpath -u "`$vswhere -requires Microsoft.Component.MSBuild -property installationPath`"/MSBuild/*/bin/MSBuild.exe)"
      fi
      ;;
@@ -56,10 +70,10 @@ assemblies=""
 for i in Pomona.UnitTests Pomona.SystemTests Pomona.IndependentClientTests Pomona.SystemTests.ClientCompatibility;
 do
     assembly=tests/$i/bin/$CONFIG/$i.dll
-    if [ ! -f "$assembly" ]; then
-        "$MSBUILD" Pomona.sln //p:Configuration=$CONFIG
+    if [ ! -f "$assembly" ] || [ "$ALWAYS_BUILD" = 1 ]; then
+        "$MSBUILD" Pomona.sln $ARG_PREFIX/p:Configuration=$CONFIG
     fi
     assemblies="$assemblies $assembly"
 done
 
-$DOTNET $runner $assemblies TestResult.$CONFIG.xml --where "$PREDICATE" --labels=All
+$DOTNET $runner $assemblies --result TestResult.$CONFIG.xml --where "$PREDICATE" --labels=All $NUNIT_EXTRA_ARGS
